29e0f3dfecfb6fc5a130721f2f804b34374c7b14
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2022-07-19 18:54:34 +0430

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ from lxml import etree
 src = "./src/web_page.html"
 tree = etree.parse(src)
 tree_paragraph = tree.xpath("//title/text()")[0]
-tree_list = tree.findall("body/ul/li")
+tree_hello = tree.xpath("//p/text()")[0]
 string_tree = etree.tostring(tree)
 
 print(string_tree)
+print(tree_hello)
 print(tree_paragraph)
 
-for li in tree_list:
-    a = li.find("a")
-    if a is not None:
-        print(f"{li.text.strip()} {a.text}")
-    else:
-        print(li.text)
+for i in range(0, 2):
+    tree_list = tree.xpath("//li/text()")[i]
+    print(tree_list)
