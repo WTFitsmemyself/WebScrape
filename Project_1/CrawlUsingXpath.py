3b04b40c6fab7306d63de4db9ae53cacc0134e74
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2022-07-27 15:22:46 +0430

    update

--- Project_1/CrawlUsingXpath.py
+++ Project_1/CrawlUsingXpath.py
@@ -6,8 +6,8 @@ print(tree)
 tree_paragraph = tree.xpath("//title/text()")[0]
 tree_hello = tree.xpath("//p/text()")[0]
 string_tree = etree.tostring(tree)
-
 li_list = tree.xpath("//li")
+
 for li in li_list:
     lisT = ''.join(map(str.strip, li.xpath(".//text()")))
     print(lisT)
