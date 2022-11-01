fcb5e565aaf115faa23dd1a11b276214b2d2554b
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2022-11-01 22:30:02 +0330

    Update main.py

--- BTC/Second_Method/main.py
+++ BTC/Second_Method/main.py
@@ -1,7 +1,12 @@
 from bitcoinaddress import Wallet
 import csv
 
+key_file_name = "found_wallets.txt"
+mode_key_file = "a"
 new_list = []
+curr_key = 19000182
+last_key = 115792089237316195423570985008687907853269984665640564039457584007913129639935
+
 with open('wallets.csv', newline='') as f:
     reader = csv.reader(f)
     data = list(reader)
@@ -10,42 +15,37 @@ for i in range(0, 2000):
     new_Data = data[i][0]
     new_list.append(new_Data)
 
-current_num_key = 7858130
-last_key = 115792089237316195423570985008687907852837564279074904382605163141518161494336
 
-for i in range(current_num_key, last_key):
+def write_to_a_file(filename, mode):
+    f = open(filename, mode)
+    f.write(str(wallet))
+    f.close()
+
+
+def print_status(key):
+    print('------------------------------------------------------')
+    print(key)
+    print('------------------------------------------------------')
+
+
+for i in range(curr_key, last_key):
     new = "{:064X}".format(i)
     wallet = Wallet(new)
     # print(wallet)
     print(f"Num KEY = {i}")
     if wallet.address.mainnet.pubaddr3 in new_list:
-        f = open("found_wallets.txt", "a")
-        f.write(str(wallet))
-        f.close()
-        print(wallet)
-        print('------------------------------------------------------')
+        write_to_a_file(key_file_name, mode_key_file)
+        print_status(wallet)
     elif wallet.address.mainnet.pubaddr1 in new_list:
-        f = open("found_wallets.txt", "a")
-        f.write(str(wallet))
-        f.close()
-        print(wallet)
-        print('------------------------------------------------------')
+        write_to_a_file(key_file_name, mode_key_file)
+        print_status(wallet)
     elif wallet.address.mainnet.pubaddr1c in new_list:
-        f = open("found_wallets.txt", "a")
-        f.write(str(wallet))
-        f.close()
-        print(wallet)
-        print('------------------------------------------------------')
+        write_to_a_file(key_file_name, mode_key_file)
+        print_status(wallet)
     elif wallet.address.mainnet.pubaddrbc1_P2WPKH in new_list:
-        f = open("found_wallets.txt", "a")
-        f.write(str(wallet))
-        f.close()
-        print(wallet)
-        print('------------------------------------------------------')
+        write_to_a_file(key_file_name, mode_key_file)
+        print_status(wallet)
     elif wallet.address.mainnet.pubaddrbc1_P2WSH in new_list:
-        f = open("found_wallets.txt", "a")
-        f.write(str(wallet))
-        f.close()
-        print(wallet)
-        print('------------------------------------------------------')
+        write_to_a_file(key_file_name, mode_key_file)
+        print_status(wallet)
     i += 1
